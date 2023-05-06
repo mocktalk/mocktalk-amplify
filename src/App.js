d61d77ffab7a49a061905b70a16121a2e35aa6b4
daikonk <Nicholas.Stoneking24@gmail.com>
2023-05-05 20:23:26 -0700

    ball

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React balls
+          Learn React ball
         </a>
       </header>
     </div>
