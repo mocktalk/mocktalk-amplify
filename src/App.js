70be8924dfa6ade1879a232c7b977d981d7c68df
mocktalk <mocktalkai@gmail.com>
2023-05-07 03:19:25 -0700

    PR test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React ball
+          Learn React PR test
         </a>
       </header>
     </div>
